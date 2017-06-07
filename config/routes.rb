29a9a0df95cda0777af546528f27bb49aef9b417
shijunfeng <johnshiluck@gmail.comgit>
2017-06-07 08:14:37 +0800

    implemet step 5

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,17 @@ Rails.application.routes.draw do
 
   resources :jobs
 
+
+
   namespace :admin do
-    resources :jobs
+    resources :jobs do
+      member do
+        post :publish
+        post :hide
+      end
+    end
   end
-  
+
   root "jobs#index"
 
 end
